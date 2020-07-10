3d160b9b8d3b027eca933727753b83665c1fabc9
daniel21h <hesseldaniel21@gmail.com>
2020-07-09 22:52:29 -0300

    feat: Card in Activities component finalized

--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
 
 import avatar from '../../images/avatar.png';
 
-import { Container, Header, Title, Card, CardHeader, Avatar, Description, Bold } from './styles';
+import {
+  Container,
+  Header,
+  Title,
+  Card,
+  CardHeader,
+  Avatar,
+  Description,
+  Bold,
+  CardBody,
+  UserName,
+  CardFooter,
+  Details,
+  Value,
+  Divider,
+  Date,
+  Actions, 
+  Option,
+  OptionLabel,
+} from './styles';
 
 export default function Activities() {
   return (
@@ -18,6 +38,33 @@ export default function Activities() {
             <Bold>Você</Bold> pagou a <Bold>@danielhessel</Bold>
           </Description>
         </CardHeader>
+
+        <CardBody>
+          <UserName>Daniel Hessel</UserName>
+        </CardBody>
+
+        <CardFooter>
+          <Details>
+            <Value>R$ 50,00</Value>
+
+            <Divider />
+
+            <Feather name="lock" size={14} color="#fff" />
+            <Date>há 3 horas</Date>
+          </Details>
+
+          <Actions>
+            <Option>
+              <MaterialCommunityIcons name="comment-outline" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+
+            <Option>
+              <Feather name="heart" size={14} color="#fff" />
+              <OptionLabel>0</OptionLabel>
+            </Option>
+          </Actions>
+        </CardFooter>
       </Card>
     </Container>
   )
